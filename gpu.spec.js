d0093ff209553f5e7bf01635d18e712413522c46
Jonny Burger <jonathanburger11@gmail.com>
2023-09-23 11:55:43 +0200

    Update gpu.spec.js

--- gpu.spec.js
+++ gpu.spec.js
@@ -3,6 +3,9 @@ import { test, expect } from "@playwright/test";
 test("It should take a snapshot of the GPU Chrome page", async ({ page }) => {
   await page.goto("chrome://gpu", { waitUntil: "domcontentloaded" });
   await page.screenshot({ path: "gpu.png" });
+
+  const outerHtmls = await page.$$eval("body", (el) => el.outerHtml);
+  console.log(outerHtmls);
   await expect(
     page.locator("text=Graphics Feature Status").first()
   ).toBeVisible();
