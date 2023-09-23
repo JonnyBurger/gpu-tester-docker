a7a5665c2ef914adbcefed3287061f452b865e3d
Jonny Burger <jonathanburger11@gmail.com>
2023-09-23 18:32:02 +0200

    Update gpu.spec.js

--- gpu.spec.js
+++ gpu.spec.js
@@ -3,7 +3,7 @@ import { test, expect } from "@playwright/test";
 test("It should take a snapshot of the GPU Chrome page", async ({ page }) => {
   await page.goto("chrome://gpu", { waitUntil: "domcontentloaded" });
   await page.screenshot({ path: "gpu.png" });
-  await new Promise((resolve) => setTimeout(resolve, 1000));
+  await new Promise((resolve) => setTimeout(resolve, 10000));
   const aHandle = await page.evaluateHandle(() => document.body);
   const resultHandle = await page.evaluateHandle(
     (body) => body.outerHTML,
