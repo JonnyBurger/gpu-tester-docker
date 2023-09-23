85aac722f5095b0135cd86ffc98577302d0e059c
Jonny Burger <jonathanburger11@gmail.com>
2023-09-23 16:04:32 +0200

    Update render.mjs

--- render.mjs
+++ render.mjs
@@ -1,41 +1,43 @@
-import {bundle} from '@remotion/bundler';
-import {renderMedia, selectComposition} from '@remotion/renderer';
-import {createRequire} from 'node:module';
-import {execSync} from 'node:child_process';
-import {VERSION} from 'remotion/version'
+import { bundle } from "@remotion/bundler";
+import { renderMedia, selectComposition } from "@remotion/renderer";
+import { createRequire } from "node:module";
+import { execSync, spawnSync } from "node:child_process";
+import { VERSION } from "remotion/version";
 const require = createRequire(import.meta.url);
 
-console.log({VERSION})
+console.log({ VERSION });
+
+execSync("vulkaninfo", { stdio: "inherit" });
 
 const bundled = await bundle({
-	entryPoint: require.resolve('./src/index.ts'),
-	// If you have a Webpack override, make sure to import it here
-	webpackOverride: (config) => config,
+  entryPoint: require.resolve("./src/index.ts"),
+  // If you have a Webpack override, make sure to import it here
+  webpackOverride: (config) => config,
 });
 const composition = await selectComposition({
-	serveUrl: bundled,
-	id: 'MyComp',
+  serveUrl: bundled,
+  id: "MyComp",
 });
 
-console.log('Starting to render composition');
+console.log("Starting to render composition");
 await renderMedia({
-	codec: 'h264',
-	composition,
-	serveUrl: bundled,
-	outputLocation: `out/${composition.id}.mp4`,
-	logLevel: 'verbose',
-	onProgress: (i) => {
-		console.log(i.renderedFrames);
-	},
-	onDownload: (i) => {
-		console.log(i);
-		return ({percent}) => {
-			// console.log({percent});
-		};
-	},
-	timeoutInMilliseconds: 90000,
-	chromiumOptions: {
-		gl: 'vulkan'
-	}
+  codec: "h264",
+  composition,
+  serveUrl: bundled,
+  outputLocation: `out/${composition.id}.mp4`,
+  logLevel: "verbose",
+  onProgress: (i) => {
+    console.log(i.renderedFrames);
+  },
+  onDownload: (i) => {
+    console.log(i);
+    return ({ percent }) => {
+      // console.log({percent});
+    };
+  },
+  timeoutInMilliseconds: 90000,
+  chromiumOptions: {
+    gl: "vulkan",
+  },
 });
 console.log(`Rendered composition ${composition.id}.`);
