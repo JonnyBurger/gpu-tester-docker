d446e6fe36e65b4f413d8ee86bc55f81eb13b87d
Jonny Burger <jonathanburger11@gmail.com>
2023-09-23 11:57:34 +0200

    hi

--- gpu.spec.js
+++ gpu.spec.js
@@ -4,7 +4,7 @@ test("It should take a snapshot of the GPU Chrome page", async ({ page }) => {
   await page.goto("chrome://gpu", { waitUntil: "domcontentloaded" });
   await page.screenshot({ path: "gpu.png" });
 
-  const outerHtmls = await page.$$eval("body", (el) => el.outerHtml);
+  const outerHtmls = await page.content();
   console.log(outerHtmls);
   await expect(
     page.locator("text=Graphics Feature Status").first()
