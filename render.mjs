4d2f32e84a9774621f2aa93d7927d3b25a5059ef
Jonny Burger <jonathanburger11@gmail.com>
2023-09-23 16:26:19 +0200

    Update render.mjs

--- render.mjs
+++ render.mjs
@@ -37,7 +37,7 @@ await renderMedia({
   },
   timeoutInMilliseconds: 90000,
   chromiumOptions: {
-    gl: "vulkan",
+    gl: "angle",
   },
 });
 console.log(`Rendered composition ${composition.id}.`);
